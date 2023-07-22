bc46f6dfe92559205c1d2931f99f3ee40c71af32
Basil M Kuriakose <basilmon92@gmail.com>
2023-07-23 00:10:34 +0530

    Improved by refactoring into two smaller components

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,20 +1,13 @@
 import Title from "./Title";
 import {services} from "../data"
+import Service from "./Service";
 const Services = () => {
     return (
         <section className="section services" id="services">
             <Title title="our" subtitle="services"/>
             <div className="section-center services-center">
-                {services.map(({id,icon,title,text})=>{
-                    return <article className="service" key={id}>
-                        <span className="service-icon"><i className={icon}></i></span>
-                        <div className="service-info">
-                            <h4 className="service-title">{title}</h4>
-                            <p className="service-text">
-                                {text}
-                            </p>
-                        </div>
-                    </article>
+                {services.map((service)=>{
+                    return <Service key={service.id} {...service}/>
                 })}
             </div>
         </section>
--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,33 +1,14 @@
 import Title from "./Title";
 import {tours} from "../data"
+import Tour from "./Tour";
 const Tours = () => {
     return (
         <section className="section" id="tours">
             <Title title="featured" subtitle="tours"/>
 
             <div className="section-center featured-center">
-                {tours.map(({id,image,date,title,info,location,duration,cost})=>{
-                    return <article className="tour-card" key={id}>
-                        <div className="tour-img-container">
-                            <img src={image} className="tour-img" alt=""/>
-                            <p className="tour-date">{date}</p>
-                        </div>
-                        <div className="tour-info">
-                            <div className="tour-title">
-                                <h4>{title}</h4>
-                            </div>
-                            <p>
-                                {info}
-                            </p>
-                            <div className="tour-footer">
-                                <p>
-                                    <span><i className="fas fa-map"></i></span> {location}
-                                </p>
-                                <p>{duration} days</p>
-                                <p>from ${cost}</p>
-                            </div>
-                        </div>
-                    </article>
+                {tours.map((tour)=>{
+                    return <Tour key={tour.id} {...tour}/>
                 })}
             </div>
         </section>
